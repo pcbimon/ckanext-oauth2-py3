8e49e1214aa100534a88a56615b4847e101e864d
Patipat C. <patipat.che@mahidol.edu>
2024-07-12 23:26:04 +0700

    feat: check user is dict

--- ckanext/oauth2/plugin.py
+++ ckanext/oauth2/plugin.py
@@ -190,7 +190,7 @@ class OAuth2Plugin(plugins.SingletonPlugin):
         user_obj = User.by_name(login)
         if not user_obj:
             user_obj = User.by_email(login)
-
+        log.debug('User extra is dict %s',isinstance(user_obj.plugin_extras, dict)) # type: ignore
         if user_obj is None:
             log.debug('Login failed - username or email %r not found', login)
         elif not user_obj.is_active:
